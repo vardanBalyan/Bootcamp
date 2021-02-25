3f5f334fc08507b54ad9981e1c113ab6d0f2e1a2
vardanttn <78465870+vardanttn@users.noreply.github.com>
2021-02-25 19:36:09 +0530

    Add files via upload

---
+++ Maven-Bootcamp/src/main/java/App.java
@@ -0,0 +1,13 @@
+import org.jboss.system.*;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public int calculateSomething() {
+        return 0;
+    }
+}
